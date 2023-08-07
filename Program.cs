fadb685de32900e7f67a76d4f319a555a7eb4166
lucasmarquesDev <lunatec09@gmail.com>
2023-08-07 13:00:53 -0300

    corrigindo autenticacao

--- Program.cs
+++ Program.cs
@@ -72,9 +72,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthentication();
-app.UseAuthorization();
-
 var summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
